77c52cfaced672dd57c2fa6e3b890b437c760f1f
Sungmin Hong <68056524+hskhsmm@users.noreply.github.com>
2026-04-05 19:55:04 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        while(true) {
+            if(N % 5 == 0) {
+                count += (N / 5);
+                System.out.println(count);
+                break;
+            }
+            //5로 안떨어지면 3kg 봉지
+            N -= 3;
+            count++;
+
+            if(N < 0) {
+                System.out.println("-1");
+                break;
+            }
+        }
+
+    }
+}
